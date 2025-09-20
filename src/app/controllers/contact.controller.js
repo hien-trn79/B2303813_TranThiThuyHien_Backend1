93cf2603352c2fe28716f0dfca47b84fd669e6ed
hintran_05 <you@example.com>
2025-09-20 21:23:22 +0700

    Xu ly cac route

--- src/app/controllers/contact.controller.js
+++ src/app/controllers/contact.controller.js
@@ -1,28 +1,29 @@
 class contact {
+    // [POST] /api/contacts/
     create (req, res, next) {
         res.send('handler create')
     }
-
+    // [GET] /api/contacts/
     findAll (req, res, next) {
         res.send('handler findAll')
     }
-
+    // [GET] /api/contacts/:id
     findOne (req, res, next) {
         res.send('handler findOne')
     }
-
+    // [PUT] /api/contacts/:id
     update (req, res, next) {
         res.send('handler update')
     }
-
+    // [DELETE] /api/contacts/:id
     delete (req, res, next) {
         res.send('handler create')
     }
-
+    // [DELETE] /api/contacts/
     deleteAll (req, res, next) {
         res.send('handler deleteAll')
     }
-
+    // [GET] /api/contacts/favorite
     findAllFavorite (req, res, next) {
         res.send('handler findAllFavorite')
     }
